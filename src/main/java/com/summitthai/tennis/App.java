345344c959081120f17a0e8120cc801a4c405521
tonalpha <tonchocolate@gmail.com>
2021-04-29 10:28:01 +0700

    Init project

---
+++ src/main/java/com/summitthai/tennis/App.java
@@ -0,0 +1,13 @@
+package com.summitthai.tennis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
